5a2464840fe79be96feaef119febebb2577207d6
Vova <vovabliahar@gmail.com>
2019-08-18 20:36:58 +0200

    css+method(push and join)

--- start-react/src/Car/Car.js
+++ start-react/src/Car/Car.js
@@ -1,10 +1,23 @@
 import React from"react"
+import "./Car.css"
  const car=(props)=>{
+     const inputClasses = ["input"]
+        if (props.name!==""){
+            inputClasses.push("green")
+        }
+        else{
+            inputClasses.push("red")
+        }
      return(
-        <div>
+        <div className="Car">
             <h1>Car name: {props.name}</h1>
             <p>Year: {props.year}</p>
-            <input type="text" onChange={props.changeCar}/>
+            <input 
+            type="text" 
+            onChange={props.changeCar} 
+            value={props.name}
+            className={inputClasses.join(" ")}
+            />
             <button onClick={props.clickDelite}>Delite</button>
             
         </div>
